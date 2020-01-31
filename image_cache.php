f3c4af0a40fe1ce835c9441ebc9063f5fca3f97d
azukiazusa1 <cycleippei@gmail.com>
2020-01-31 10:26:40 +0900

    no message

--- image_cache.php
+++ image_cache.php
@@ -1,6 +1,6 @@
 <?php
 $url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon";
-$dir_path = "/Users/A19021/Pokomeon-get-Adventure/src/assets/images";
+$dir_path = "YOUT_PATH";
 $result = json_decode($response,true);
 for ($i = 1; $i <= 493; $i++) {
 	$response = file_get_contents("{$url}/shiny/{$i}.png");
